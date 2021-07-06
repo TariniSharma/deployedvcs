0202a570f7a9ce59824e5d91f07eaec6ab782702
Tarini Sharma <73395976+TariniSharma@users.noreply.github.com>
2021-07-07 00:33:44 +0530

    Add files via upload

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,52 +1,93 @@
 //p2p connection establishment 
 //variables declaration ---
 const localVideo = document.getElementById("localUserVideo");
-const remoteVideo = document.getElementById("remoteUserVideo");
+const remoteVideo1 = document.getElementById("remoteUserVideo1");
+const remoteVideo2 = document.getElementById("remoteUserVideo2");
+const remoteVideo3 = document.getElementById("remoteUserVideo3");
+const remoteVideo4 = document.getElementById("remoteUserVideo4");
+const remoteVideo5 = document.getElementById("remoteUserVideo5");
+const remoteVideo6 = document.getElementById("remoteUserVideo6");
+const remoteVideo7 = document.getElementById("remoteUserVideo7");
+const remoteVideo8 = document.getElementById("remoteUserVideo8");
 const constraints = {'video': true, 'audio': false };
 const config = {'iceServers': [{'urls': 'stun:stun.l.google.com:19302'}]};
 
+var connections =[];
+var currentInCall = [];
 const {RTCPeerConnection, RTCSessionDescription} = window;
-const peerConnection = new RTCPeerConnection(config);
+//2 remote users
+connections.push(new RTCPeerConnection(config));
+connections.push(new RTCPeerConnection(config));
+connections.push(new RTCPeerConnection(config));
+connections.push(new RTCPeerConnection(config));
+connections.push(new RTCPeerConnection(config));
+connections.push(new RTCPeerConnection(config));
+connections.push(new RTCPeerConnection(config));
+connections.push(new RTCPeerConnection(config));
+let usersInCall = 0;
+//const peerConnection = new RTCPeerConnection(config);
 const socket = io.connect();//change this
-console.log("socket: "+socket);
 let called = false;
 let repeating = false;
+let joiningSecondaryPeers = false;
 //called - false initially, this is to make sure I dont keep calling callUser(data.socket) infinitely. To establish p2p connection, I need to callUser(socketId) and callUser(data.socket)
 //repeating - false initially, this is to make sure that the dialog asking that user A is calling accept? is asked only once as we are calling callUser twice
 
 //local stream reachable
 localVideo.addEventListener('loadedmetadata', function() {
     console.log("Got local stream");
-})
+});
 //remote stream reachable
-remoteVideo.addEventListener('loadedmetadata', function() {
-    console.log("Got remote stream");
-})
+remoteVideo1.addEventListener('loadedmetadata', function() {
+    console.log("Got remote stream1");
+});
+remoteVideo2.addEventListener('loadedmetadata', function() {
+    console.log("Got remote stream2");
+});
+remoteVideo3.addEventListener('loadedmetadata', function() {
+    console.log("Got remote stream3");
+});
+remoteVideo4.addEventListener('loadedmetadata', function() {
+    console.log("Got remote stream4");
+});
+remoteVideo5.addEventListener('loadedmetadata', function() {
+    console.log("Got remote stream5");
+});
+remoteVideo6.addEventListener('loadedmetadata', function() {
+    console.log("Got remote stream6");
+});
+remoteVideo7.addEventListener('loadedmetadata', function() {
+    console.log("Got remote stream7");
+});
+remoteVideo8.addEventListener('loadedmetadata', function() {
+    console.log("Got remote stream8");
+});
+
 
 //socket sock-it!
-socket.on("update-user-list", ({users}) => {
-    updateUserList(users);
+socket.on("update-user-list", ({users, keys, vals}) => {
+    updateUserList(users, keys, vals);
 });
 
 socket.on("call-made", async data => {
+    //adding new connection to called side
     if(!repeating)
     {
+        //connections.push(new RTCPeerConnection(config));
         const confirmed = window.confirm(
             `User "Socket: ${data.socket}" wants to call. Do you accept this call?`
         );
         if(!confirmed)
         {
-            console.log("call rejected!");
             socket.emit("reject-call", {
                 from : data.socket
             });
             return;
         }
     }
-        
-    await peerConnection.setRemoteDescription(new RTCSessionDescription(data.offer));
-    const answer = await peerConnection.createAnswer();
-    await peerConnection.setLocalDescription(new RTCSessionDescription(answer));
+    await connections[usersInCall].setRemoteDescription(new RTCSessionDescription(data.offer));
+    const answer = await connections[usersInCall].createAnswer();
+    await connections[usersInCall].setLocalDescription(new RTCSessionDescription(answer));
 
     socket.emit("make-answer", {
         answer,
@@ -58,15 +99,56 @@ socket.on("call-made", async data => {
 });
 
 socket.on("answer-made", async data => {
-    await peerConnection.setRemoteDescription(new RTCSessionDescription(data.answer));
-    console.log("all steps done");
+    await connections[usersInCall].setRemoteDescription(new RTCSessionDescription(data.answer));
     if(!called)
     {
-        console.log("here too");
         callUser(data.socket);
         called = true;
+        //quotaFinished = true;
+    }
+    else
+    {
+        currentInCall.push(data.socket);
+        usersInCall = usersInCall+1;
+        repeating = false;
+        called = false;
+        //housekeeping other
+        socket.emit("housekeep", {
+            from : data.socket
+        });
+        if(joiningSecondaryPeers===false)
+        {
+            socket.emit("connect-to-others", { //should i have this when I am already in connecting-to-others. no
+                currentInCall,
+                to : data.socket
+            });
+        }
     }
 });
+
+socket.on("connecting-to-others", async data => {
+    let toJoin = data.currentInCall;
+    joiningSecondaryPeers = true;
+    for (let i = 0; i<toJoin.length; i++)
+    {
+        if(socket.id !== toJoin[i])
+        {
+            //change
+            //usersInCall = 1;
+            await callUser(toJoin[i]);
+            //usersInCall = usersInCall+1;
+            await timer(7000); 
+        }
+    }
+    joiningSecondaryPeers = false;
+});
+const timer = ms => new Promise(res => setTimeout(res, ms))
+
+socket.on("housekeeped", data => {
+    called = false;
+    repeating = false;
+    usersInCall = usersInCall+1;
+})
 
 socket.on("call-rejected", data => {
     alert(`User: "Socket: ${data.socket}" rejected your call`);
@@ -81,13 +163,26 @@ socket.on("remove-user", ({socketId}) => {
     }
 });
 
-peerConnection.ontrack = function({streams : [stream]}) {
-    const remoteVideo = document.getElementById("remoteUserVideo");
-    if(remoteVideo)
-    {
-        remoteVideo.srcObject = stream;
-    }
-};
+
+for(let i = 0;i<connections.length;i++)
+{
+    let idx = i;
+    connections[idx].ontrack = function({streams : [stream]}) {
+        let temp = "remoteUserVideo"+(idx+1);
+        const remoteVideo = document.getElementById(temp);
+        if(remoteVideo)
+        {
+           remoteVideo.srcObject = stream;
+        }
+    
+    };
+}
+//for (let i=0;i<n;i++)
+//{
+//   (closure){} => applying i to something //only i = n would be applied
+//}
+//instead assign idx = i at the beginning
+
 
 navigator.getUserMedia(constraints, stream => {
     const localVideo = document.getElementById("localUserVideo");
@@ -95,7 +190,12 @@ navigator.getUserMedia(constraints, stream => {
     {
         localVideo.srcObject = stream;
     }
-    stream.getTracks().forEach(track => peerConnection.addTrack(track, stream));
+    for(let i=0;i<connections.length;i++)
+    {
+        let idx = i;
+        stream.getTracks().forEach(track => connections[idx].addTrack(track, stream));
+    }
+     console.log("tracks: "+stream.getTracks());
     },
     error => {
         console.log(error.message);
@@ -105,19 +205,33 @@ navigator.getUserMedia(constraints, stream => {
 
 
 //helper functions
-function updateUserList(socketIds) 
+function updateUserList(socketIds, keys, vals) 
 {
     const activeUserContainer = document.getElementById("activeUsers");
     socketIds.forEach(socketId => {
         const alreadyExisitingUser = document.getElementById(socketId);
         if(!alreadyExisitingUser)
         {
-            const userContainerEl = createUserItemContainer(socketId);
+            let userName = getKey(keys, vals, socketId);
+            console.log("username: "+keys+":"+vals+":"+socketId);
+            const userContainerEl = createUserItemContainer(socketId, userName);
             activeUserContainer.appendChild(userContainerEl);
         }
     });
 }
-function createUserItemContainer(socketId)
+function getKey(keys, vals, searchValue) 
+{
+    for (let ind = 0; ind<vals.length; ind++)
+    {
+        console.log("f:"+vals[ind]+"-"+searchValue);
+        if(vals[ind] === searchValue)
+        {
+            return keys[ind];
+        }
+    }
+}
+  
+function createUserItemContainer(socketId, userName)
 {
     const userContainerEl = document.createElement("div");
     const usernameEl = document.createElement("p");
@@ -125,7 +239,7 @@ function createUserItemContainer(socketId)
     userContainerEl.setAttribute("class", "active-user");//necessary?
     userContainerEl.setAttribute("id", socketId);
     usernameEl.setAttribute("class", "username");
-    usernameEl.innerHTML = `Socket: ${socketId}`;
+    usernameEl.innerHTML = `User: ${userName}`;
 
     userContainerEl.appendChild(usernameEl);
 
@@ -149,11 +263,16 @@ function unselectUsersFromList()
 }
 async function callUser(socketId)
 {
-    const offer = await peerConnection.createOffer();
-    await peerConnection.setLocalDescription(new RTCSessionDescription(offer));//D
+    console.log("call user "+socketId);
+    //append peer connection to connections
+    //pushed new peer connection from caller side. to do from called side as well SIDELINING WHAT HAPPENS WHEN CALL IS REJECTED FOR NOW
+    //connections.push(new RTCPeerConnection(config));
+    const offer = await connections[usersInCall].createOffer();
+    await connections[usersInCall].setLocalDescription(new RTCSessionDescription(offer));//D
+    console.log("before emiting");
     socket.emit("call-user", {
         offer,
         to : socketId
     });
+    console.log("after emiting");
 }
-  
--- server.js
+++ server.js
@@ -8,10 +8,17 @@ var express_1 = __importDefault(require("express"));
 var socket_io_1 = require("socket.io");
 var http_1 = require("http");
 var path_1 = __importDefault(require("path"));
+
+require('dotenv').config();
+const { auth, requiresAuth } = require('express-openid-connect');
+
 var Server = /** @class */ (function () {
     function Server() {
         this.port = process.env.PORT || 3000;
         this.activeSockets = [];
+        this.activeUsers = []; //Constraint: user nickname must be unique => user email id must be unique
+        this.hash = new Map();
+        this.newUser;
         this.init();
         this.configureApp();
         this.handleRoutes();
@@ -21,14 +28,45 @@ var Server = /** @class */ (function () {
         this.app = express_1.default();
         this.httpServer = http_1.createServer(this.app);
         this.io = new socket_io_1.Server(this.httpServer);
+        this.app.use(
+            auth({
+              authRequired: false,
+              auth0Logout: true,
+              issuerBaseURL: process.env.ISSUER_BASE_URL,
+              baseURL: process.env.BASE_URL,
+              clientID: process.env.CLIENT_ID,
+              secret: process.env.SECRET,
+              //idpLogout: true,
+            })
+        );
     };
     Server.prototype.configureApp = function () {
         this.app.use(express_1.default.static(path_1.default.join(__dirname, "/public")));
     };
     Server.prototype.handleRoutes = function () {
-        this.app.get("/", function (req, res) {
+        var _this = this;
+        this.app.get("/", requiresAuth(), function (req, res) {
+            //console.log(req.oidc.user.nickname + " has logged in");
+            //console.log(this);
+            //add this user to list of existing users 
+            var existingUser = _this.activeUsers.find(function (existingUser) { return existingUser === req.oidc.user.nickname; });
+            if(!existingUser)
+            {
+                _this.activeUsers.push(req.oidc.user.nickname);
+            }
+            //del previous instance from hash
+            if(_this.hash.has(req.oidc.user.nickname))
+            {
+                _this.hash.delete(req.oidc.user.nickname);
+            }
+            _this.newUser = req.oidc.user.nickname;
+            //console.log("users in system:" + _this.activeUsers);
+
             var options = { root: path_1.default.join(__dirname, "./public") };
             res.sendFile("webpage.html", options);
+        });
+        this.app.get('/profile', requiresAuth(), (req,res) => {
+            res.send(JSON.stringify(req.oidc.user));
         });
     };
     Server.prototype.listen = function (callback) {
@@ -40,21 +78,56 @@ var Server = /** @class */ (function () {
     Server.prototype.handleSocketConnections = function () {
         var _this = this;
         this.io.on("connection", function (socket) {
+           // console.log("connected socket");
             var existingSocket = _this.activeSockets.find(function (existingSocket) { return existingSocket === socket.id; });
             if (!existingSocket) {
                 _this.activeSockets.push(socket.id);
+                if(_this.newUser)
+                {
+                    _this.hash.set(_this.newUser, socket.id);
+                    _this.newUser = null;
+                }
+                // console.log("hash: ");
+                // for (let [key, value] of _this.hash.entries()) {
+                //     console.log(key + ' = ' + value)
+                //   }
+                //make keys and vals array
+                let keys = [];
+                let vals = [];
+                let c =0;
+                for (let [key, value] of _this.hash.entries()) {
+                    keys[c] = key;
+                    vals[c] = value;
+                    c++;
+                  }
                 //send to client the list of all other sockets except self
                 socket.emit("update-user-list", {
-                    users: _this.activeSockets.filter(function (existingSocket) { return existingSocket !== socket.id; })
+                    users: _this.activeSockets.filter(function (existingSocket) { return existingSocket !== socket.id; }),
+                    keys: keys,
+                    vals: vals
+                    //hash: _this.hash
                 });
                 //send to all other sockets the socket id of self
                 socket.broadcast.emit("update-user-list", {
-                    users: [socket.id]
+                    users: [socket.id],
+                    keys: keys,
+                    vals: vals
                 });
             }
             socket.on("call-user", function (data) {
                 socket.to(data.to).emit("call-made", {
                     offer: data.offer,
+                    socket: socket.id
+                });
+            });
+            socket.on("connect-to-others", function (data) {
+                socket.to(data.to).emit("connecting-to-others", {
+                    currentInCall: data.currentInCall,
+                    socket: socket.id
+                });
+            });
+            socket.on("housekeep", function (data) {
+                socket.to(data.from).emit("housekeeped", {
                     socket: socket.id
                 });
             });
