0d8dcfd80e67df68ae3abdd2431694b402c72917
Tarini Sharma <73395976+TariniSharma@users.noreply.github.com>
2021-07-13 22:08:10 +0530

    Add files via upload

--- test.js
+++ test.js
@@ -1,8 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+
+//create server instance
 var server_1 = require("./server");
 var server = new server_1.Server();
+
 server.listen(function (port) {
-    console.log("listening man");
+    console.log("Server is listening on https://testbootstrapheroku.herokuapp.com/");
 });
-//# sourceMappingURL=test.js.map
